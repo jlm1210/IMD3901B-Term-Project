2eb7f34cecb17077afa202b84239b955ec2e5d90
Ellena Tzavelas <wakandacat@gmail.com>
2024-04-10 12:55:21 -0400

    key and door works

--- src/worlds/CometCreations/js/object-generation.js
+++ src/worlds/CometCreations/js/object-generation.js
@@ -1,6 +1,7 @@
 //function to populate interactable objects
 function setupObjects(){
 
+    //setup the phone and its code input UI
     var phone = document.querySelector("#Phone");
     var posX = -0.2;
     var posZ = 0.2;
@@ -9,6 +10,7 @@ function setupObjects(){
     var rot = 90;
     codeManager(phone, posX, posY, posZ, scale, rot);
 
+    //setup the cabinet and its code input UI
     var cabinet = document.querySelector("#Cabinet");
     cabinet.setAttribute('class', 'inter');
     cabinet.setAttribute('circles-interactive-object', {type: "highlight", highlight_color: accessCol});
@@ -20,6 +22,33 @@ function setupObjects(){
     codeManager(cabinet, posX, posY, posZ, scale, rot);
     codeEvents();
 
+    //make the second door interactable
+    var door2 = document.querySelector("#medDoor2");
+    door2.setAttribute('class', 'inter');
+    door2.setAttribute('circles-interactive-object', {type: "highlight", highlight_color: accessCol});
+    door2.addEventListener('click', function() {
+
+        //check if the player is holding the key
+        var player = CIRCLES.getMainCameraElement();
+        var allChildren = player.children;
+        for (var i = 0; i < allChildren.length; i++) {
+            if(allChildren[i].id == "key"){
+                //open the door
+                var sound = document.querySelector('#doorOpen');
+                sound.components.sound.playSound();
+                var doorPivot = document.querySelector("#doorParent2");
+                doorPivot.setAttribute('rotation', "0 90 0");
+
+                //remove the key from the players hand
+                var key = document.querySelector("#key");
+                player.removeChild(key);
+            }
+        }
+    });
+
+
+
+    //setup the welcome text
     const welcome = document.getElementById('welcome_description');
     welcome.setAttribute('circles-description', {title_text_front:'Comet Creations', description_text_front:`${jsonData.HUBText.Welcome}`, description_text_back:`${jsonData.HUBText.Welcome}`, lookAtCamera:true});
 
@@ -65,9 +94,13 @@ function setupObjects(){
 
             if(!alreadyPicked){
                 //put the info into the UI
+                var container = document.createElement("div");
+                container.setAttribute('class', "objectText");
+
                 //title
                 var newEl = document.createElement("h4");
                 newEl.innerHTML = currentObj.title;
+                container.appendChild(newEl);
              
                 //description
                 var innerEl = document.createElement("h6");
@@ -75,7 +108,7 @@ function setupObjects(){
                 innerEl.innerHTML = currentObj.desc;
                 innerEl.setAttribute('style', 'display: none;');
                 newEl.appendChild(innerEl);
-                document.querySelector("#info").appendChild(newEl);
+                document.querySelector("#info").appendChild(container);
 
                 //button
                 var newButton = document.createElement("button"); 
@@ -84,7 +117,7 @@ function setupObjects(){
                 newButton.addEventListener("click", function() {
                     showDesc(currentObj.title);
                 });
-                document.querySelector("#info").appendChild(newButton);
+                container.appendChild(newButton);
 
             }
 
@@ -170,15 +203,24 @@ function codeEvents(){
     }
 }
 
+//check if the code inputs are correct or not
 function codeCheck(id){
     if(id.includes("Phone")){
-        if(phoneGuess.length === 4){
-            if(phoneGuess === phoneCode){
-                for(let i = 0; i < phoneCode.length; i++){
+        if(phoneGuess.length === 4){ //once 4 inputs have been made
+            if(phoneGuess === phoneCode){ //if its correct
+                for(let i = 0; i < phoneCode.length; i++){ //change the correct code to be green
                     document.querySelectorAll('.code')[phoneCode[i]].setAttribute('material', {color: 'rgb(0, 255, 0)'}, {side: 'double'});
                 }
+                //give the player the key
+                var key = document.querySelector("#key");
+                key.setAttribute('visible', true);
+                key.setAttribute('position', "0.5 -0.5 -1");
+                key.flushToDOM();
+                var copy = key.cloneNode();
+                var player = CIRCLES.getMainCameraElement();
+                player.appendChild(copy);
                 
-            } else {
+            } else { //if its wrong
                 phoneGuess = "";
                 for(let i = 0; i < 10; i++){
                     document.querySelectorAll('.code')[i].setAttribute('material', {color: 'rgb(0, 0, 0)'}, {side: 'double'});
@@ -186,16 +228,15 @@ function codeCheck(id){
             }
         }
     } else {
-        if(cabinetGuess.length === 4){
-            if(cabinetGuess === cabinetCode){
-                for(let i = 0; i < cabinetCode.length; i++){
+        if(cabinetGuess.length === 4){ //once 4 inputs have been made
+            if(cabinetGuess === cabinetCode){ //if its correct
+                for(let i = 0; i < cabinetCode.length; i++){ //change the correct code to be green
                     var r = Number(cabinetCode[i]);
                     r = r + 10;
-                    console.log(r);
                     document.querySelectorAll('.code')[r].setAttribute('material', {color: 'rgb(0, 255, 0)'}, {side: 'double'});
                 }
                 
-            } else {
+            } else { //if its wrong
                 cabinetGuess = "";
                 for(let i = 10; i < 20; i++){
                     document.querySelectorAll('.code')[i].setAttribute('material', {color: 'rgb(0, 0, 0)'}, {side: 'double'});
