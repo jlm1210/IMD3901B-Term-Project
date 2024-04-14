b50eaed6f2b841a39b106ed261701bbad0fba401
Ellena Tzavelas <wakandacat@gmail.com>
2024-04-14 18:07:38 -0400

    pickup working and plates made

--- src/worlds/CometCreations/js/object-generation.js
+++ src/worlds/CometCreations/js/object-generation.js
@@ -22,6 +22,45 @@ function setupObjects(){
     codeManager(cabinet, posX, posY, posZ, scale, rot);
     codeEvents();
 
+    //setup the interactable medicines
+    for(let i = 0; i < 4; i++){
+        let med = document.querySelector(`#medicine${i+1}`);
+        med.setAttribute('class', 'inter');
+        med.setAttribute('circles-interactive-object', {type: "highlight", highlight_color: accessCol});
+
+        //add a click function to pickup
+        med.addEventListener('click', function() {
+            med.setAttribute('visible', false);
+            med.removeEventListener('click', arguments.callee);
+            med.flushToDOM();
+            var copy = med.cloneNode();
+            var player = CIRCLES.getMainCameraElement();
+            player.appendChild(copy);
+            copy.setAttribute('visible', true);     
+            copy.setAttribute('position', "0.5 -0.5 -1");
+        });
+    }
+
+    //setup the interactable plates
+    for(let i = 0; i < 4; i++){
+        let plate = document.querySelector(`#plate${i+1}`);
+        plate.setAttribute('class', 'inter');
+        plate.setAttribute('circles-interactive-object', {type: "highlight", highlight_color: accessCol});
+
+        //add a click function to pickup
+        plate.addEventListener('click', function() {
+
+            var player = CIRCLES.getMainCameraElement();
+            var allChildren = player.children;
+            for (var i = 0; i < allChildren.length; i++) {
+                if(allChildren[i].id.includes("medicine")){
+                    console.log("put down");
+                }
+            }
+        });
+    }
+
+
     //make the second door interactable
     var door2 = document.querySelector("#medDoor2");
     door2.setAttribute('class', 'inter');
